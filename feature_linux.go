0436ed1a8727c0941338321dea2aceada8b281b6
DaPoHou <xmlf@hotmail.com>
2020-12-07 10:20:28 +0800

    Update feature_linux.go

--- feature_linux.go
+++ feature_linux.go
@@ -5,6 +5,6 @@ import (
 	//_ "github.com/nadoo/glider/service/dhcpd"
 
 	// comment out the protocols you don't need to make the compiled binary smaller.
-	//_ "github.com/nadoo/glider/proxy/redir"
+	_ "github.com/nadoo/glider/proxy/redir"
 	//_ "github.com/nadoo/glider/proxy/unix"
 )
